a296d56a44052fb04c38ca1dfea7644cdab43540
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-11-13 13:53:47 +0900

    modify file
    add statement list

--- ginza_mama_bot/plugins/slack.py
+++ ginza_mama_bot/plugins/slack.py
@@ -4,7 +4,7 @@ import sys
 from slackbot.bot import respond_to, listen_to
 
 # sys.path.append( "../../jubatus/" )
-from .jubatus.juba_analyze import recommend_Komachi
+from jubatus.juba_analyze import recommend_Komachi
 
 class Reply:
     def __init__(self):
@@ -17,9 +17,10 @@ r = Reply()
 
 @listen_to('(.*)')
 def refrection(message, something):
+    statements.append(something)
     count = r.increase()
     if count % 5 == 0:
-        recommend = recommend_Komachi(something)
+        recommend = recommend_Komachi(statements)
         print(recommend)
         message.reply(*recommend)
     else:
