696e1d0da1fc3147ad3f1f3767d5b9fb1d70203f
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-11-13 12:22:33 +0900

    make python package
    slackbot

---
+++ slackbot/plugins/slack.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from slackbot.bot import respond_to, listen_to
+
+# sys.path.append( "../../jubatus/" )
+from .jubatus.juba_analyze import recommend_Komachi
+
+class Reply:
+    def __init__(self):
+        self.counter = 0
+
+    def increase(self):
+        self.counter += 1
+        return self.counter
+r = Reply()
+
+@listen_to('(.*)')
+def refrection(message, something):
+    count = r.increase()
+    if count % 5 == 0:
+        recommend = recommend_Komachi(something)
+        print(recommend)
+        message.reply(*recommend)
+    else:
+        message.reply('それで？')
